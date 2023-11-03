a5c86e97408c256bd267b48285b395214e95b41c
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-03 19:00:00 -0300

    Create SlotVerify.mjs

---
+++ SlotVerify.mjs
@@ -0,0 +1,11 @@
+export function SlotVerify(ArrayDeSlots){
+  let Lista1 = ArrayDeSlots[0];
+  let Lista2 = ArrayDeSlots[1];
+  let Lista3 = ArrayDeSlots[2];
+
+  let a1 = 0;
+  while(a1<Lista1.length){
+    console.log(Lista1[a1]);
+    a++;
+  }
+}
