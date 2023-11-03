7aa4fc90a31c7152d9402aca1ea5edca142574eb
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-03 16:53:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods","GET, POST");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Authorization");
+    console.log('Valor postado', req.body);
     next();
 })
 
