206f1f85ed5331614e7620d159ebd3138f7bdb5a
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-03 12:00:11 -0300

    Update slot.mjs

--- slot.mjs
+++ slot.mjs
@@ -1,3 +1,79 @@
+let count = 3;
 export function getEmoji(){
-return 9;
+	/*if(ModoTurbo){
+		count=3;
+	}else{
+		count=30;
+	}*/
+	//return count;
+	
+	let list_1 = [];
+	//let list_2 = [];
+	//let list_3 = [];
+	
+	let a1 = 0;
+	while(a1<count){
+		list_1.push(getEmoji());
+		a1++;
+	}
+	/*
+	let a2 = 0;
+	while(a2<count){
+		list_2.push(getEmoji());
+		a2++;
+	}
+	
+	let a3 = 0;
+	while(a3<count){
+		list_3.push(getEmoji());
+		a3++;
+	}*/
+	
+	return list_1;
+}
+
+function getEmoji(){
+	let minimo = 1;
+	let maximo = 10;
+	
+	let randomEmoji = 
+	Math.floor(
+	Math.random()
+	* 
+	(maximo - minimo +1)
+	) 
+	+ 
+	minimo;
+	
+	if(randomnumber==1){
+	  return "image/po.png";
+	  }
+	  if(randomnumber==2){
+	  return "image/tai_lung.png";
+	  }
+	  if(randomnumber==3){
+	  return "image/mestre_shifu.png";
+	  }
+	  if(randomnumber==4){
+	  return "image/tigressa.png";
+	  }
+	  if(randomnumber==5){
+	  return "image/louva_deus.png";
+	  }
+	  if(randomnumber==6){
+	  return "image/oogway.png";
+	  }
+	  if(randomnumber==7){
+	  return "image/mestre_macaco.png";
+	  }
+	  if(randomnumber==8){
+	  return "image/garca.png";
+	  }
+	  if(randomnumber==9){
+	  return "image/mestre_vibora.png";
+	  }
+	  
+	  if(randomnumber==10){
+	  return "image/manopla.png";
+	  }
 }
