f23d5685ba44972330493f64099166ee8408ae15
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-03 17:31:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods","GET, POST");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Authorization");
-    console.log('Valor postado', req.body);
     next();
 })
 
@@ -37,18 +36,8 @@ app.get("/", function (req, res) {
     res.send("Olá, Biel! " + databaseUrl);
 });
 
-app.get("/A", function (req, res) {
-    res.send("Retorno"+saldo);
-    console.log("AAAAAAAAAA");
-});
-
-app.get("/B", function (req, res) {
-    res.send("B");
-});
-
 app.get("/Slot", function (req, res) {
-    console.log('Valor postado', req.body);
-   
+console.log('Valor postado', req.body);
 res.send(""+getEmoji());
 });
 
