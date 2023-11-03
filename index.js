e8e2e03a7e9cb66bb1dfc6f1e313849a1fbaf5fa
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-02 23:38:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const port = process.env.PORT || 3000;
 
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods","GET");
+    res.header("Access-Control-Allow-Methods","GET, POST");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Authorization");
-});
+    next();
+})
+
 app.get("/", function (req, res) {
     res.send("Olá, Biel! " + databaseUrl);
 });
