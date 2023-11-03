60289a21c93bbcc7f913416030473496732e4627
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-03 19:14:52 -0300

    Update SlotVerify.mjs

--- SlotVerify.mjs
+++ SlotVerify.mjs
@@ -1,3 +1,5 @@
+let StringListArray = "G:";
+export { StringListArray }
 export function Verify(ArrayDeSlots){
   let Lista1 = ArrayDeSlots[0];
   let Lista2 = ArrayDeSlots[1];
@@ -5,13 +7,24 @@ export function Verify(ArrayDeSlots){
 
   let a1 = 0;
   while(a1<Lista1.length){
-    console.log(Lista1[a1]);
+   // console.log(Lista1[a1]);
+    StringListArray = StringListArray+Lista1[a1]);
     a1++;
   }
 
   let a2 = 0;
   while(a2<Lista2.length){
-    console.log(Lista2[a2]);
+   // console.log(Lista2[a2]);
     a2++;
   }
+
+  let a3 = 0;
+  while(a3<Lista3.length){
+   // console.log(Lista3[a3]);
+    a3++;
+  }
+
+  console.log(StringListArray);
+  
+  
 }
