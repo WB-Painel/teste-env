72ad9b525a019bb97726755e98f1aa5b6c44a40b
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-05 13:36:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
 
-require('./routes');
+
 
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin","*");
